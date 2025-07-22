2650085e858c94e1ec7f8f05924de2eb16510e32
Shubham Srivastava <99933579+surajvast1@users.noreply.github.com>
2025-07-22 23:54:46 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,4 +1,6 @@
 def call(String Projectname,String ImageTag,String Dockerhubuser)
 {
+  echo "This is building the code"
   sh "docker build -t ${Dockerhubuser}/${Projectname}:${ImageTag} ."
+  echo "Code building success"
 }
